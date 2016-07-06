5eadf8f56ffcfbbf78f11afcea25ed696b0ed1ed
Adrian Hartanto <s.adrianh@gmail.com>
2016-07-07 03:07:23 +0700

    Fix babel

---
+++ dist_modules/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _sidedrawer = require('./sidedrawer');
+
+var _sidedrawer2 = _interopRequireDefault(_sidedrawer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _sidedrawer2.default;
